f214d35c566ac1fb6b53c7a30d3aaa6a265977eb
antonio.kudric <antonio4.kudric@gmail.com>
2025-08-12 15:42:52 +0200

    All origins added

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,17 +1,13 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://glittering-lolly-940dc3.netlify.app'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      credentials: true,
-      keepHeadersOnError: true,
+      origin: '*', // Allow all origins
     },
   },
+  'strapi::security',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
